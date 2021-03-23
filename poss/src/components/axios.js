6145206e78aaa8bd42cfcd57106c967a36c6e5c4
GitVijayDv <80312247+GitVijayDv@users.noreply.github.com>
2021-03-23 20:06:11 +0530

    up url2
    up

--- poss/src/components/axios.js
+++ poss/src/components/axios.js
@@ -2,7 +2,6 @@ import axios from "axios";
 
 export default axios.create({
   baseURL: 'http://ec2-3-239-92-6.compute-1.amazonaws.com/:4000/api',
-  // baseURL: `http://162.241.71.139:4000/api/v1/`,
   headers: {
     'Access-Control-Allow-Origin' : '*',
     'Access-Control-Allow-Methods':'GET,PUT,POST,DELETE,PATCH,OPTIONS',
