5b05fa0cc71cda45515525c401b96897435f0281
GitVijayDv <onlinemeeting.1211@gmail.com>
2021-03-23 20:03:56 +0530

    up api url

--- poss/src/components/axios.js
+++ poss/src/components/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: 'http://ec2-3-234-221-223.compute-1.amazonaws.com:4000/',
+  baseURL: 'http://ec2-3-239-92-6.compute-1.amazonaws.com/:4000/api',
   // baseURL: `http://162.241.71.139:4000/api/v1/`,
   headers: {
     'Access-Control-Allow-Origin' : '*',
