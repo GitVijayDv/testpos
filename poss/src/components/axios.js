ee96e7f348159ea63ab428d3563accf0b049620f
GitVijayDv <80312247+GitVijayDv@users.noreply.github.com>
2021-03-28 20:10:46 +0530

    Update axios.js
    upd url

--- poss/src/components/axios.js
+++ poss/src/components/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: 'http://ec2-3-239-92-6.compute-1.amazonaws.com:4000/',
+  baseURL: 'http://ec2-34-239-167-142.compute-1.amazonaws.com:4000/',
   headers: {
     'Access-Control-Allow-Origin' : '*',
     'Access-Control-Allow-Methods':'GET,PUT,POST,DELETE,PATCH,OPTIONS',
